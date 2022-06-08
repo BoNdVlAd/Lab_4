bd5e8f84eabc5e7e2a7c35c908ce840f389726e3
Vlad <wlad23072003@gmail.com>
2022-06-08 22:14:50 +0300

    finall

--- src/src/Main.java
+++ src/src/Main.java
@@ -5,9 +5,10 @@ import java.util.Comparator;
 class Plane {
     String name;
     String color;
+    int pilots;
     int age;
     int weight;
-    Plane(String name, String color, int age, int weight) {
+    Plane(String name, String color,int pilots, int age, int weight) {
         this.name = name;
         this.color = color;
         this.age = age;
@@ -18,6 +19,9 @@ class Plane {
     }
     String getColor() {
         return color;
+    }
+    int getPilots() {
+        return pilots;
     }
     int getAge() {
         return age;
@@ -33,9 +37,9 @@ class Plane {
 public class Main {
     public static final ArrayList<Plane> planes = new ArrayList<>();
     public static void main(String[] args) {
-        planes.add(new Plane("«Мрія»", "white",  34, 285000 ));
-        planes.add(new Plane("Airbus 380", "yellow",  17, 276800 ));
-        planes.add(new Plane("Ан-2", "green",  45, 3300 ));
+        planes.add(new Plane("«Мрія»", "white",2,  34, 285000 ));
+        planes.add(new Plane("Airbus 380", "yellow",2,  17, 276800 ));
+        planes.add(new Plane("Ан-2", "green",2,  45, 3300 ));
 
         System.out.println("Сортування по віку(за збільшенням) літаків");
         planes.sort(Comparator.comparing(Plane::getAge));
